9cc1b5dab191cc31cf65c7a6a14c133875ed5ede
Alexander Zuev <zuev08@gmail.com>
2019-02-11 19:36:36 +0300

    Basic application structure

---
+++ src/main/java/com/example/javaproject/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package com.example.javaproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value={"/"})
+    public String getIndexPage(Model model) {
+        return "index";
+    }
+}
