fdc4b1185c932d2e81dd6a3749e379c8641b4df9
Alexander Zuev <zuev08@gmail.com>
2019-02-13 12:29:24 +0300

    Show Role, Username, Login, Logout in nav on layout.html

---
+++ src/main/java/com/example/javaproject/services/TemplateHelper.java
@@ -0,0 +1,7 @@
+package com.example.javaproject.services;
+
+public interface TemplateHelper {
+    boolean isLoggedIn();
+    boolean isAdmin();
+    String getUsername();
+}
---
+++ src/main/java/com/example/javaproject/services/TemplateHelperImpl.java
@@ -0,0 +1,35 @@
+package com.example.javaproject.services;
+
+import com.example.javaproject.data.RoleRepository;
+import com.example.javaproject.models.Role;
+import com.example.javaproject.models.RoleName;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TemplateHelperImpl implements TemplateHelper {
+
+    private final SecurityService securityService;
+    private final Role adminRole;
+
+    @Autowired
+    public TemplateHelperImpl(SecurityService securityService, RoleRepository roleRepository) {
+        this.securityService = securityService;
+        adminRole = roleRepository.findByName(RoleName.ADMIN.name);
+    }
+
+    @Override
+    public boolean isLoggedIn() {
+        return securityService.findLoggedInUsername() != null;
+    }
+
+    @Override
+    public boolean isAdmin() {
+        return securityService.findLoggedInUser().getRoles().contains(adminRole);
+    }
+
+    @Override
+    public String getUsername() {
+        return securityService.findLoggedInUsername();
+    }
+}
