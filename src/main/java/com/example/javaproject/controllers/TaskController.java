40799758a84bea089e90330263e27dd3291cc78b
Alexander Zuev <zuev08@gmail.com>
2019-02-17 12:29:00 +0300

    Add change Assinged To Field form

--- src/main/java/com/example/javaproject/controllers/TaskController.java
+++ src/main/java/com/example/javaproject/controllers/TaskController.java
@@ -1,23 +1,28 @@
 package com.example.javaproject.controllers;
 
 import com.example.javaproject.models.Task;
+import com.example.javaproject.models.User;
 import com.example.javaproject.services.TaskService;
+import com.example.javaproject.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Controller
 public class TaskController {
     private final TaskService taskService;
+    private final UserService userService;
 
     @Autowired
-    public TaskController(TaskService taskService) {
+    public TaskController(TaskService taskService, UserService userService) {
         this.taskService = taskService;
+        this.userService = userService;
     }
 
     @RequestMapping(value = {"/tasks"}, method = RequestMethod.GET)
@@ -53,5 +58,31 @@ public class TaskController {
     public String saveTask(@ModelAttribute("task") Task task, BindingResult bindingResult, Model model) {
         taskService.save(task);
         return "redirect:/tasks";
+    }
+
+    @RequestMapping(value = {"/tasks/{id}/editAssignedTo"}, method = RequestMethod.GET)
+    public String getEditAssignedToForm(Model model, @PathVariable("id") Long id) {
+        Task task = taskService.findById(id);
+        List<User> users = userService.getAll();
+        Map<Long, User> values = users.stream().collect(Collectors.toMap(
+                User::getId,
+                Function.identity(),
+                (e1, e2) -> e2,
+                LinkedHashMap::new
+        ));
+        Long selectedId = task.getAssignedTo() != null ? task.getAssignedTo().getId() : null;
+        int selectedIndex = -1;
+        int i = 0;
+        for (User user : users) {
+            if (Objects.equals(user.getId(), selectedId)) {
+                selectedIndex = i;
+            }
+            i++;
+        }
+        model.addAttribute("create", false);
+        model.addAttribute("task", task);
+        model.addAttribute("values", values);
+        model.addAttribute("checkedIndex", selectedIndex);
+        return "tasks/task_edit_assignedTo";
     }
 }
